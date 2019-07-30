c2bc9c6bdde725ad011e17318637296deb2c086c
Lari Alakukku <lari.alakukku@aalto.fi>
2019-07-30 13:17:48 +0300

    Removed unnecessary userType field

--- models/authModels.js
+++ models/authModels.js
@@ -11,10 +11,7 @@ const authOptionSchema = Schema({
 //This Schema is used to tie together documents associated with each login strategy.
 const userSchema = Schema({
   privileges: { type: String, required: true, enum: ['student', 'assistant', 'teacher', 'maintainer'], default: 'student' }, //Highest level of accounts is chosen
-  aliases: [{
-     id: { type: ObjectID, required: true },
-     userType: { type: String, required: true, enum: ['local', 'dummy', 'haka'] }
-  }],
+  aliases: [{ type: ObjectID, required: true }],
 });
 
 //An alias corresponds to a login method. Each alias has to have at least these properties;
